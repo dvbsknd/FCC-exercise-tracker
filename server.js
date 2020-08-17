44324bb5e93510a47f970da196495698bda2382e
Dave Baskind <dave@picketstudio.com>
2020-08-18 07:57:33 +1000

    Swap body-parser for native express

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
 
 const cors = require('cors')
 
@@ -10,8 +9,8 @@ console.log(process.env.MONGO_URI);
 
 app.use(cors())
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
 
 
 app.use(express.static('public'))
