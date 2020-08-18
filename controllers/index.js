066b11e34d1c949dc1333e981c7a07c4436484d0
Dave Baskind <dave@picketstudio.com>
2020-08-18 17:35:57 +1000

    Add exercises to a User

--- controllers/index.js
+++ controllers/index.js
@@ -28,14 +28,27 @@ module.exports = {
         });
       });
   },
-  addExercise: function (req, res) {
-    User.find()
-      .then(users => res.json({ users }))
+  addExercise: (req, res, next) => {
+    const {
+      userId,
+      description,
+      duration } = req.body;
+    const date = req.body.date || undefined;
+    const exercise = {
+      description,
+      duration,
+      date
+    };
+    const options = {
+      new: true
+    }
+    User.findByIdAndUpdate(userId, { $push: { exercises: exercise } }, options)
+      .then(user => res.json({ user }))
       .catch(err => {
         console.error(err);
         res.status(500).json({
           error: {
-            message: 'Error while trying to retrieve users',
+            message: 'Error while trying to update user',
             error: err
           }
         });
--- models/user.model.js
+++ models/user.model.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, {
   dbName: process.env.MONGO_DB,
   useUnifiedTopology: true,
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true
 });
-
-mongoose.set('useCreateIndex', true);
 
 const exerciseSchema = new mongoose.Schema({
   description: { type: String, required: true },
