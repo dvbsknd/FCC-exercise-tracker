34754941318fd752e7b196db6b6207523b1ca1e4
Dave Baskind <dave@picketstudio.com>
2020-08-18 20:38:49 +1000

    Unnest user object being returned

--- controllers/index.js
+++ controllers/index.js
@@ -44,7 +44,7 @@ module.exports = {
       new: true
     }
     User.findByIdAndUpdate(userId, { $push: { exercises: exercise } }, options)
-      .then(user => res.json({ user }))
+      .then(user => res.json(user))
       .catch(err => {
         console.error(err);
         res.status(500).json({
